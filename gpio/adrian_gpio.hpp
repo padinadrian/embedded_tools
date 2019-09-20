d1754334c6dca6d8a767b49aa9bbc8dee23e2c4d
adrianp <adrianp@wolfram.com>
2019-09-20 15:38:16 -0500

    New GPIO interface and initial Arduino implementation.

---
+++ gpio/adrian_gpio.hpp
@@ -0,0 +1,74 @@
+/**
+ * File:    adrian_gpio.hpp
+ * Author:  padin.adrian@gmail.com
+ *
+ * Copyright 2019
+ */
+
+#ifndef ADRIAN_GPIO_HPP_
+#define ADRIAN_GPIO_HPP_
+
+/* ===== Includes ===== */
+#include "../adrian_helpers.hpp"
+
+namespace adrian
+{
+    /**
+     * GPIO Interface
+     * Implement this interface for each new platform.
+     * This interface can then be used for any project
+     * which uses the adrian::GPIO class.
+     */
+    class GPIO
+    {
+    public:
+        /* ===== Enums ===== */
+        enum PinMode
+        {
+            PIN_MODE_INPUT,
+            PIN_MODE_OUTPUT,
+            PIN_MODE_INPUT_PULLUP,
+        };
+
+        /* ===== Functions ===== */
+
+        /** Mandatory virtual destructor. */
+        virtual ~GPIO() {};
+
+        /** Set the pin as an input or output. */
+        virtual void SetPinMode(const PinMode mode) = 0;
+
+        /**
+         * Set the state of the pin.
+         * Pin must be configured as an OUTPUT first.
+         * Writing to an INPUT pin may result in undefined behavior.
+         */
+        virtual void Write(const bool state) const = 0;
+
+        /**
+         * Read the state of the pin.
+         * Pin must be configured as an INPUT first.
+         * Read from an OUTPUT pin may result in undefined behavior.
+         */
+        virtual bool Read() const = 0;
+
+        /** Operator form of Write. */
+        void operator=(const bool state) const
+        {
+            this->Write(state);
+        }
+
+        /**
+         * Operator form of Read.
+         * Uses the "Safe Bool Idiom"
+         * See: https://www.artima.com/cppsource/safebool.html
+         */
+        bool operator!() const
+        {
+            return !this->Read();
+        }
+    };
+
+}   // end namespace adrian
+
+#endif  // ADRIAN_GPIO_HPP_
---
+++ gpio/adrian_gpio_arduino.hpp
@@ -0,0 +1,70 @@
+/**
+ * File:    adrian_gpio_arduino.hpp
+ * Author:  padin.adrian@gmail.com
+ *
+ * Copyright 2019
+ */
+
+#ifndef ADRIAN_GPIO_ARDUINO_HPP_
+#define ADRIAN_GPIO_ARDUINO_HPP_
+
+/* ===== Includes ===== */
+#include <Arduino.h>
+#include "adrian_gpio.hpp"
+
+namespace adrian
+{
+    /**
+     * GPIO Interface
+     * Implement this interface for each new platform.
+     * This interface can then be used for any project
+     * which uses the adrian::GPIO class.
+     */
+    class ArduinoGPIO : public adrian::GPIO
+    {
+    public:
+        /* ===== Functions ===== */
+
+        /** Constructor */
+        ArduinoGPIO(const int pin_number) : m_pin_number(pin_number)
+        {
+            // Nothing
+        }
+
+        /** Set the state of the pin. */
+        virtual void SetPinMode(const PinMode mode)
+        {
+            switch (mode)
+            {
+                case PIN_MODE_OUTPUT:
+                    pinMode(pin_number, INPUT);
+                    break;
+                case PIN_MODE_INPUT_PULLUP:
+                    pinMode(pin_number, INPUT);
+                    break;
+                case PIN_MODE_INPUT:
+                default:    // fall-through
+                    pinMode(pin_number, INPUT);
+                    break;
+            }
+        }
+
+        /** Set the state of the pin. */
+        virtual void Write(const bool state) const
+        {
+            digitalWrite(m_pin_number, state ? HIGH : LOW)
+        }
+
+        /** Read the state of the pin. */
+        virtual bool Read() const
+        {
+            return (digitalRead(m_pin_number) == HIGH);
+        }
+
+    private:
+        const int m_pin_number;
+    };
+
+}   // end namespace adrian
+
+#endif  // ADRIAN_GPIO_ARDUINO_HPP_
